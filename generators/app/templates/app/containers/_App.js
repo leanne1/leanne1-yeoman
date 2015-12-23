bef09793baa2dbd9bb949d74194808031115daa8
leanne <leanne.tite@gmail.com>
2015-12-23 20:16:22 +0000

    App container boilerplate

---
+++ generators/app/templates/app/containers/_App.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fooActions } from '../actions/';
+
+export class App extends Component { 
+    static propTypes = {}
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const {} = this.props;
+        return (
+            <main role="main">
+                <h1></h1>
+            </main>
+        );
+    }
+}
+
+/**
+ * Props that the container receives from redux state.
+ */
+function mapStateToProps() {
+    return {};
+}
+
+/**
+ * Action props that the container receives bound to redux dispatch
+ */
+function mapDispatchToProps(dispatch) {
+    return {
+        fooActions: bindActionCreators(fooActions, dispatch),
+    };
+}
+
+// Make redux-react binding
+export default connect(mapStateToProps(), mapDispatchToProps)(App);
